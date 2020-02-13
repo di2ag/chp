5e29b77721e33dffdbb59d7bf3385574ef503d63
GregHydeDarmouth <Gregory.M.Hyde.TH@dartmouth.edu>
2020-02-13 14:11:15 -0500

    patientBKFProcessor class for processing TCGA data. Has example under main on how to use

---
+++ core/patientBKFProcessor.py
@@ -0,0 +1,171 @@
+import csv
+from pybkb import bayesianKnowledgeBase as BKB
+from pybkb import BKB_S_node, BKB_component, BKB_I_node
+import matplotlib.pyplot as plt
+
+class PatientProcessor:
+    def __init__(self):
+        # each patient BKF
+        self.bkfs = list()
+        self.patients = list()
+        self.clinicalCollected = False
+
+    class Patient:
+        # will need to account for mutatedReads
+        def __init__(self, patID, cancer, mutatedGenes):
+            # patient gene data
+            self.patientID = patID
+            self.cancerType = cancer
+            self.mutatedGenes = mutatedGenes
+            self.mutatedGeneReads = list()
+            self.geneReadsStats = list() #implement
+
+            # clinical later
+            self.ageDiagnos = -1
+            self.gender = ""
+            self.survivalTime = -1
+
+            # add drug info later
+            # add radiation info later
+
+    # processes patient gene data
+    def processPatientGeneData(self, patientMutGenes, patientMutReads, geneReadStats):
+        print("Reading file 1 - patient genes")
+        with open(patientMutGenes, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            patientGeneDict = dict()
+            # row[0] = cancer type row[1] = patientID, row[6] = mutated gene
+            rows = [(row[0],row[1],row[6]) for row in reader]
+
+        print("Processing file 1...")
+        # create all patients
+        mutatedPatGenes = list()
+        # skip header
+        cancerType = rows[1][0]
+        patID = rows[1][1]
+        for row in rows[1:]:
+            if patID != row[1]:
+                p = self.Patient(patID,cancerType,mutatedPatGenes)
+                self.patients.append(p)
+                patID = row[1]
+                cancerType = row[0]
+                mutatedPatGenes = list()
+            mutatedPatGenes.append(row[2])
+        csv_file.close()
+
+        print("Reading file 2 - gene read stats")
+        #implement
+        print("Processing file 2...")
+        #implement
+
+        print("Reading file 3 - patient gene reads")
+        # get gene reads from tumor tissue
+
+        with open(patientMutReads, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            geneReadsDict = dict()
+            # row[1] = patientID, row[6] = gene, row[7] = reads
+            for row in reader:
+                geneReadsDict[str(row[1])+str(row[6])] = row[7]
+        print("Processing file 3...")
+        for p in self.patients:
+            geneMismatch = []
+            for gene in p.mutatedGenes[:]:
+                if str(p.patientID)+str(gene) in geneReadsDict:
+                    p.mutatedGeneReads.append(geneReadsDict[str(p.patientID)+str(gene)])
+                #gene read information not found
+                else:
+                    geneMismatch.append(gene)
+                    p.mutatedGenes.remove(gene)
+            if len(geneMismatch) > 0:
+                print("Warning - Patient", p.patientID, "has", len(geneMismatch), "missing genes")
+
+    def processClinicalData(self, clinicalData):
+        assert len(self.patients) > 0, "Patient and gene data have not been read in yet. Call processPatientGeneData(patientMutGenesFile, patientMutReadsFile) first, before secondary processing functions are called."
+        print("Reading clinical data...")
+        with open(clinicalData, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            patientClinicalDict = dict()
+            # row[0] = cancerType, row[1] = patientID, row[2] = age of diagnosis, row[3] = gender, row[7] = survival time
+            for row in reader:
+                patientClinicalDict[str(row[0])+str(row[1])] = (str(row[2]),str(row[3]),str(row[7]))
+
+        for p in self.patients:
+            if str(p.cancerType)+str(p.patientID) in patientClinicalDict:
+                clinical = patientClinicalDict[str(p.cancerType)+str(p.patientID)]
+                p.ageDiagnos = clinical[0]
+                p.gender = clinical[1]
+                p.survivalTime = clinical[2]
+            else:
+                print("WARNING - Patient", p, "clinical data does not exist")
+        self.clinicalCollected = True
+        print("Clinical data processed")
+
+    # should be called after all Patient objects have been cosntructed
+    def processPatientBKF(self):
+        print("Writing BKF files...")
+        assert len(self.patients) > 0, "Have not processed Patient and gene data yet."
+
+        for pat in self.patients:
+            bkf = BKB(name=pat.patientID + '_BKF')
+
+            for gene in pat.mutatedGenes:
+                # gene
+                mutGeneComp = BKB_component("mut_" + gene + "=")
+                iNodeGeneMut = BKB_I_node('True',mutGeneComp)
+                mutGeneComp.addINode(iNodeGeneMut)
+                bkf.addComponent(mutGeneComp)
+
+                # stat condition bin
+                statConditionComp = BKB_component("mu-STD>=" + gene + "<=mu+STD=")
+                statConditionTrue = BKB_I_node('True',statConditionComp)
+                statConditionFalse = BKB_I_node('False',statConditionComp)
+                statConditionComp.addINode(statConditionTrue)
+                statConditionComp.addINode(statConditionFalse)
+                bkf.addComponent(statConditionComp)
+
+                # form SNode  o---->[mut_<genename>=True]
+                bkf.addSNode(BKB_S_node(mutGeneComp, iNodeGeneMut, 1.0))
+
+                if True: #add stat condition given data, i.e. compare patient gene reads vs the population mean and std
+                    # form SNode  [mut_<genename>=True]----o---->[mu-STD>=<genename><=mu+STD=True
+                    bkf.addSNode(BKB_S_node(statConditionComp, statConditionTrue, 1.0, [(mutGeneComp, iNodeGeneMut)]))
+                    # form SNode  [mut_<genename>=True]----o---->[mu-STD>=<genename><=mu+STD=False
+                    bkf.addSNode(BKB_S_node(statConditionComp, statConditionFalse, 0.0, [(mutGeneComp, iNodeGeneMut)]))
+                else:
+                    # form SNode  [mut_<genename>=True]----o---->[mu-STD>=<genename><=mu+STD=True
+                    bkf.addSNode(BKB_S_node(statConditionComp, statConditionTrue, 0.0, [(mutGeneComp, iNodeGeneMut)]))
+                    # form SNode  [mut_<genename>=True]----o---->[mu-STD>=<genename><=mu+STD=False
+                    bkf.addSNode(BKB_S_node(statConditionComp, statConditionFalse, 1.0, [(mutGeneComp, iNodeGeneMut)]))
+
+            self.bkfs.append(bkf)
+
+    def BKFsToFile(self, outDirect):
+        for i in range(0, len(self.bkfs)):
+            #bkf = BKB(name=pat.patientID + '_BKF')
+            assert self.bkfs[i].name.split("_")[0] == self.patients[i].patientID, "Data name mismatch"
+            self.bkfs[i].save(outDirect + self.bkfs[i].name)
+
+            #form patient data
+            patientData = "Patient ID:" + self.patients[i].patientID  + "\nCancer Type:" + self.patients[i].cancerType + "\nPatient Genes:"
+            for gene in self.patients[i].mutatedGenes:
+                patientData += gene + ","
+            patientData = patientData[:len(patientData)-1]
+
+            #form clinical data
+            if self.clinicalCollected:
+                patientData += "\nAge at diagnosis:" + self.patients[i].ageDiagnos + "\nGender:" + self.patients[i].gender + "\nSurvival time:" + self.patients[i].survivalTime
+
+            with open('patientBKFs/' + self.bkfs[i].name, 'r+') as f:
+                content = f.read()
+                f.seek(0,0)
+                f.write(patientData.rstrip('\r\n') + '\nBKF:\n' + content)
+            #G = self.bkfs[i].makeGraph(layout='neato')
+            #plt.savefig(outDirect + self.bkfs[i].name, format='PNG')
+
+if __name__ == '__main__':
+    PP = PatientProcessor()
+    PP.processPatientGeneData('data/wxs.csv', 'data/rnaseq_fpkm_uq_primary_tumor.csv', 'data/geneReadsStats.csv')
+    PP.processClinicalData('data/clinical.csv')
+    PP.processPatientBKF()
+    PP.BKFsToFile('patientBKFsPNGs/')
