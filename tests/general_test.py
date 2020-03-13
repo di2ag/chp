b9c6cebaa1d2ab55599758a7773f9c66e7655134
yakaboskic <yakaboskic@gmail.com>
2020-03-13 00:37:00 -0400

    General test check in.

---
+++ tests/general_test.py
@@ -0,0 +1,45 @@
+import sys
+
+from pybkb import bayesianKnowledgeBase as BKB
+from pybkb.core.python_base.reasoning import checkMutex
+
+#!!!!!!!!!! Change to your local bkb-pathway-core directory.
+#sys.path.append('/home/ghyde/bkb-pathway-provider/core')
+sys.path.append('/home/cyakaboski/src/python/projects/bkb-pathway-provider/core')
+
+from reasoner import Reasoner
+from query import Query
+
+#-- Initalize a BKB
+fused_bkb = BKB()
+
+#-- Load in the fused bkb from our datafiles
+fused_bkb.load('/home/public/data/ncats/BabelBKBs/collapsedAll/fusion.bkb')
+
+#-- Here are the associated patient data files
+patient_data_file = '/home/public/data/ncats/BabelBKBs/collapsedAll/patient_data.pk'
+withheld_patients_file = '/home/public/data/ncats/BabelBKBs/collapsedAll/withheldPatients.csv'
+
+#-- Instiante reasoner
+reasoner = Reasoner(fused_bkb=fused_bkb)
+
+#-- Set the patient data file 
+reasoner.set_src_metadata(patient_data_file)
+
+#-- If you want to see what genetic or demographic evidence is avaliable, uncomment the line below
+#print(reasoner.metadata_ranges)
+
+#-- Make a query (evidence is for genetic info, and meta_ is for demographic info)
+query0 = Query(evidence={'mut_MAP3K3': 'True'},
+               targets=list(),
+               meta_evidence=[('Age_of_Diagnosis', '>=', 20000)],
+               meta_targets=[('Survival_Time', '>=', 300)])
+
+#-- Run the query.
+query = reasoner.analyze_query(query0)
+
+#-- Return the report
+query.getReport()
+
+#-- Check for mutex if you want to.
+#print(checkMutex(query.bkb))
