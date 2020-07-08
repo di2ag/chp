26b0228f5647bd1046fe72c6efdc1d42d4bd595a
GregHydeDarmouth <Gregory.M.Hyde.TH@dartmouth.edu>
2020-07-08 16:11:40 -0400

    my test file for unsecret agent api call

---
+++ tests/reasonerStdTests/test_unsecret_api.py
@@ -0,0 +1,15 @@
+import requests
+import os
+import pickle
+import json
+
+query_path = '/home/ghyde/bkb-pathway-provider/tests/reasonerStdTests/sample_query1.pk'
+
+if os.path.exists(query_path):
+    with open(query_path, 'rb') as f_:
+        reasoner_std = pickle.load(f_)
+
+reasoner_std['reasoner_id'] = 'unsecret'
+payload = {'query': reasoner_std}
+r = requests.post('http://129.170.69.138/submitQuery/', json=payload)
+print(r.text)
