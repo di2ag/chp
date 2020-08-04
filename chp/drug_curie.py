0f9cb90c4e58ee58b616a70787aa3adab0b7251f
GregHydeDarmouth <Gregory.M.Hyde.TH@dartmouth.edu>
2020-08-03 20:13:16 -0400

    commented drug_curie.py

--- chp/drug_curie.py
+++ chp/drug_curie.py
@@ -4,6 +4,15 @@ import requests
 import threading
 import queue
 
+
+##########################################################
+# curie_match
+# Input: drug
+# Output: string chembl id or 'NOT FOUND' if not found
+#--------------------------------------------------------
+# Description: Drug chembl ids are returned using MyChem.info
+# and must fit criteria for chembl_id.
+
 def curie_match(drug):
     url = 'http://mychem.info/v1/query?q=chembl.pref_name:{}&fields=chembl.molecule_chembl_id'.format(drug)
     r = requests.get(url)
@@ -15,22 +24,25 @@ def curie_match(drug):
         curie = 'HELP'
     return [drug,curie]
 
-drug_names_file = '/home/public/data/ncats/data_drop_03-13-2020/drug_names.csv'
 
-# reading initial patient data
-with open(drug_names_file, 'r') as csv_file:
-    reader = csv.reader(csv_file)
-    drug_names = next(reader)
-    drugs = [drug for drug in drug_names]
+if __name__ == "__main__":
 
-curie_matches = []
-for drug in drugs:
-    curie_matches.append(curie_match(drug))
+    drug_names_file = '/home/public/data/ncats/data_drop_03-13-2020/drug_names.csv'
 
-f = open('drug_curie_map.csv', 'w')
-f.write('Drug,Curie\n')
+    # reading initial patient data
+    with open(drug_names_file, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        drug_names = next(reader)
+        drugs = [drug for drug in drug_names]
 
-for d in curie_matches:
-    row = str(d[0])+','+str(d[1])+'\n'
-    f.write(row)
-f.close()
+    curie_matches = []
+    for drug in drugs:
+        curie_matches.append(curie_match(drug))
+
+    f = open('drug_curie_map.csv', 'w')
+    f.write('Drug,Curie\n')
+
+    for d in curie_matches:
+        row = str(d[0])+','+str(d[1])+'\n'
+        f.write(row)
+    f.close()
