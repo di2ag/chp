7aa3cedcf0d926e884bd8a74f8b7a648e1e337b0
GregHydeDarmouth <Gregory.M.Hyde.TH@dartmouth.edu>
2020-08-03 20:08:28 -0400

    cleaned up gene_curie.py and added drug_curie.py (not clean)

---
+++ chp/drug_curie.py
@@ -0,0 +1,36 @@
+import csv
+import tqdm
+import requests
+import threading
+import queue
+
+def curie_match(drug):
+    url = 'http://mychem.info/v1/query?q=chembl.pref_name:{}&fields=chembl.molecule_chembl_id'.format(drug)
+    r = requests.get(url)
+    r_json = r.json()
+    hits = r_json['hits']
+    if len(hits) > 0:
+        curie = 'ChEMBL:' + hits[0]['chembl']['molecule_chembl_id']
+    else:
+        curie = 'HELP'
+    return [drug,curie]
+
+drug_names_file = '/home/public/data/ncats/data_drop_03-13-2020/drug_names.csv'
+
+# reading initial patient data
+with open(drug_names_file, 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    drug_names = next(reader)
+    drugs = [drug for drug in drug_names]
+
+curie_matches = []
+for drug in drugs:
+    curie_matches.append(curie_match(drug))
+
+f = open('drug_curie_map.csv', 'w')
+f.write('Drug,Curie\n')
+
+for d in curie_matches:
+    row = str(d[0])+','+str(d[1])+'\n'
+    f.write(row)
+f.close()
